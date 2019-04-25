36d4a1fd267eeb4d92da95bcbc7ae2fb2ec7e6cf
blaine burke <blaineburke1999@gmail.com>
2019-04-25 19:31:45 +0100

    Messages

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -4,7 +4,6 @@ import {CalibratePage} from '../calibrate/calibrate';
 import { Time } from '@angular/common';
 import {Storage} from '@ionic/storage';
 
-
 @IonicPage()
 @Component({
   selector: 'page-results',
@@ -28,25 +27,11 @@ export class ResultsPage {
  va3:any;
  va4:any;
 
- va5:Date;
- va6:any;
- va7:any;
- va8:any;
+ message:String;
+ Phmessage:String;
+ 
 
- va9:Date;
- va10:any;
- va11:any;
- va12:any;
 
- va13:Date;
- va14:any;
- va15:any;
- va16:any;
-
- va17:Date;
- va18:any;
- va19:any;
- va20:any;
 
   constructor(public navCtrl: NavController,
      public navParams: NavParams,
@@ -54,104 +39,102 @@ export class ResultsPage {
      public alertCtrl: AlertController
 ) {
 
-
 }
 
   refresh()
   {
-      this.count++;   
 
-      if(this.count=1)
-      {
-        this.va5 = this.va1;
-        this.va6 = this.va2;
-        this.va7 = this.va3;
-        this.va8 = this.va4;
-      }
-      
-      if(this.count=2)
-      {
-        this.va5 = this.va1;
-        this.va6 = this.va2;
-        this.va7 = this.va3;
-        this.va8 = this.va4;
-      }
-      
-      if(this.count=3)
-      {
-        this.va9 = this.va1;
-        this.va10 = this.va2;
-        this.va11= this.va3;
-        this.va12= this.va4;
-
-        this.va5 = this.va1;
-        this.va6 = this.va2;
-        this.va7 = this.va3;
-        this.va8 = this.va4;
-      }
-
-      if(this.count=4)
-      {
-        this.va13 = this.va9;
-        this.va14 = this.va10;
-        this.va15= this.va11;
-        this.va16= this.va12;
-
-        this.va9 = this.va1;
-        this.va10 = this.va2;
-        this.va11= this.va3;
-        this.va12= this.va4;
-
-        this.va5 = this.va1;
-        this.va6 = this.va2;
-        this.va7 = this.va3;
-        this.va8 = this.va4;
-      }
-      if(this.count>=5)
-      {
-        this.va17= this.va13;
-        this.va18 = this.va14;
-        this.va19= this.va15;
-        this.va20= this.va16;
-
-        this.va13 = this.va9;
-        this.va14 = this.va10;
-        this.va15= this.va11;
-        this.va16= this.va12;
-
-        this.va9 = this.va1;
-        this.va10 = this.va2;
-        this.va11= this.va3;
-        this.va12= this.va4;
-
-        this.va5 = this.va1;
-        this.va6 = this.va2;
-        this.va7 = this.va3;
-        this.va8 = this.va4;
-      }
-
+    //Gets values from Calibrate Page
       this.storage.get("date").then((val) => {
-        this.va1 = this.date;
+       // this.va1 = this.date;
         this.date = val;
         console.log(val);
       });
+
+      //Gets values from Calibrate Page
       this.storage.get("timeKey").then((va2) => {
-        this.va2 = this.time;
+      //  this.va2 = this.time;
         this.time = va2;
         console.log(va2);
       });
+
+      //Gets values from Calibrate Page
       this.storage.get("ClKey").then((va3) => {
-        this.va3 = this.Chlorine;
+      //  this.va3 = this.Chlorine;
         this.Chlorine = va3;
         console.log( va3);
       });
+
+      //Gets values from Calibrate Page
       this.storage.get("PhKey").then((va4) => {
-        this.va4 = this.Phenol;
+      //  this.va4 = this.Phenol;
         this.Phenol = va4;
         console.log(va4);
       });
-  
+
+      //Message--------
+
+      if(this.Chlorine > 1 && this.Chlorine < 1.51)
+      {
+          this.message="Chlorine Levels are Stable!";
+          
+      }
+
+      else if(this.Chlorine > 1.5 && this.Chlorine < 2)
+      {
+          this.message="Chlorine Levels are high! Calibrate immediatly";
+      }
+
+      else if(this.Chlorine > 2)
+      {
+          this.message="Chlorine Levels are Dangerously high! Calibrate immediatly";
+      }
+
+      else if(this.Chlorine < 1 && this.Chlorine > 0.5)
+      {
+          this.message="Chlorine Levels are low Calibrate immediatly!";
+
+      }
+
+      else if(this.Chlorine < 1 && this.Chlorine > 0.5)
+      {
+          this.message="Chlorine levels are extremely low! Turn off calibrator and hand feed!";
+          
+          
+      }
+
+ /////////////////////////
+ 
+ if(this.Phenol > 7.2 && this.Phenol < 7.8)
+ {
+     this.Phmessage="Ph Levels are Stable!";
+ }
+
+ else if(this.Phenol > 7.8 && this.Phenol < 8.2)
+ {
+     this.Phmessage="Ph Levels are high! Calibrate immediatly";
+ }
+
+ else if(this.Phenol > 8.2)
+ {
+     this.Phmessage="Ph Levels are Dangerously high! Add an acid or sodium bisulphate";
+    
+ }
+
+ else if(this.Phenol < 7.2 && this.Phenol > 6.9)
+ {
+     this.Phmessage="Ph Levels are low Calibrate immediatly!";
+ }
+
+ else if(this.Phenol < 6.9)
+ {
+     this.Phmessage="Ph levels are extremely low! Add a scoop of Baking Soda! ";
      
+ }
+
+ console.log(this.message);
+ console.log(this.Phmessage);
+    
   }
 
 }
