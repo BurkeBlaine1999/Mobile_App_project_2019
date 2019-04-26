15f74ee04ba1bf275c4c6d3d18df828aef77e3cb
blaine burke <blaineburke1999@gmail.com>
2019-04-26 11:52:34 +0100

    Final Attempt

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,28 +3,39 @@ import { NavController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
 import {DataServiceProvider} from '../../providers/data-service/data-service';
-
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { ResultsPage } from '../results/results';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 
-export class HomePage implements OnInit{
+export class HomePage {
+
+  public weather :any=[];
+  public result:any = [];
 
   constructor(
     public navCtrl: NavController ,
     public alertCtrl: AlertController,
-    private DataService:DataServiceProvider ) {}
-
-  weather :any=[];
-
-    ngOnInit(){
-      this.DataService.GetWeatherData().subscribe((data)=>{
-          this.weather=data.weather;             //get the JSON data
-      });
+    private DataService:DataServiceProvider ,
+    public http: HttpClient) {
+      this.loadData();
     }
 
+    loadData()
+    {
+      let data:Observable<any>;
+      data = this.http.get('http://api.openweathermap.org/data/2.5/weather?q=Galway&APPID=17483171df536b13647eec5650e17bd7');
+      data.subscribe(result => 
+        {
+          this.weather = result.weather;
+        })
+    }
+  
   SignIn()
   {
     let alert = this.alertCtrl.create({
